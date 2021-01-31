e67b34c8e0d92ab759c99cfd8a02e0b5d1e42ac7
Jakub Voráček <jakub.voracek@gmail.com>
2021-01-31 22:25:05 +0100

    not spa

--- packages/web/nuxt.config.js
+++ packages/web/nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
-  mode: 'spa', // todo
+  // mode: 'spa', // todo
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
