e32d84672fe57f7199c61608600ea6072d4965b7
Jakub Voráček <jakub.voracek@gmail.com>
2021-05-12 19:42:51 +0200

    link based on API

--- packages/web/components/organisms/MediaCombo.vue
+++ packages/web/components/organisms/MediaCombo.vue
@@ -6,9 +6,9 @@
         <p class="mt-6 text-xl md:text-xl leading-snug">{{ content.description }}</p>
         <a
           target="_blank"
-          href="https://zazij.klubinvestoru.com"
+          :href="content.cta.link"
           class="rounded-xl mt-8 bg-green-700 text-green-100 shadow py-2 px-4 text-lg font-medium inline-flex items-center hover:shadow-2xl transition-all duration-300 hover:-translate-y-1 transform "
-        >Chci být členem →</a>
+        >{{ content.cta.text }}</a>
       </div>
       <div class="ml-auto w-6/12 rounded-xl overflow-hidden shadow-lg">
         <img
