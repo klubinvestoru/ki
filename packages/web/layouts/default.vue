62e758aebdd7e23645d6adbfed4ec44f1b193cb4
Jakub Voráček <jakub.voracek@gmail.com>
2021-12-02 18:26:11 +0100

    fix safari

--- packages/web/layouts/default.vue
+++ packages/web/layouts/default.vue
@@ -5,14 +5,10 @@
         class="w-10/12 md:container mx-auto grid gap-4 md:flex items-center justify-between flex-wrap md:flex-no-wrap md:space-x-12"
       >
         <nuxt-link to="/">
-          <img
-            src="/logo.png"
-            alt="Klub investorů"
-            class="h-8"
-          >
+          <img src="/logo.png" alt="Klub investorů" class="h-8" />
         </nuxt-link>
 
-        <nav class="flex gap-6 font-semibold text-sm text-gray-600">
+        <nav class="flex space-x-6 font-semibold text-sm text-gray-600">
           <!-- <nuxt-link to="/projekty">Projekty</nuxt-link> -->
           <nuxt-link to="/zazij">Členství</nuxt-link>
           <nuxt-link to="/kariera">Kariéra</nuxt-link>
